bef8c24dd2242ed2a0d0dc32ab01a9137eebbdaf
qsxmji <2813208126@qq.com>
2020-03-30 20:16:26 +0800

    0.1   1%开发

--- main.go
+++ main.go
@@ -36,7 +36,9 @@ var rand1 = rand.New(rand.NewSource(time.Now().UnixNano()))
 var playerds, monsterds int
 var fa1flag bool
 var fa2flag bool
-var gjbw = [14]string{"眼睛", "太阳穴", "耳", "下颏", "喉结", "面部", "颈部", "心寓", "腹部", "肋骨", "裆部", "关节", "脚背", "麻筋"}
+var gjbw = []string{"眼睛", "太阳穴", "耳", "下颏", "喉结", "面部", "颈部", "心寓", "腹部", "肋骨",
+	"裆部", "关节", "脚背", "麻筋", "脊椎", "后脑勺", "鼻", "腋窝", "脚背"}
+var yhbw = []string{"眼睛", "太阳穴", "耳", "裆部", "脊椎", "后脑勺", "心寓", "颈部"}
 
 func main() {
 
@@ -64,7 +66,6 @@ func main() {
 
 	// 创建随机数种子  (默认将1970-1-1 00:00:00当做随机数种子)
 	// 0-10之间的随机整数。 (包含0，不包含10。相当于对10求余)
-
 	fmt.Println("双方选手开始选边")
 	playerds = rand1.Intn(100)
 	monsterds = rand1.Intn(100)
@@ -73,14 +74,12 @@ func main() {
 	fmt.Println(i, i2)
 	fmt.Println(gjbw)
 	fmt.Println("双方选手上场")
-	fmt.Println("shuzu", shuzu)
 	fmt.Println("-----------战斗开始-------------")
 	if fa1flag {
 		fight1()
 	} else {
 		fight2()
 	}
-
 }
 
 func panduan(i1 int, i2 int, player Player, monster Monster) (re1 int, re2 int) {
@@ -123,75 +122,113 @@ func panduan(i1 int, i2 int, player Player, monster Monster) (re1 int, re2 int) 
 	return playerds, monsterds
 }
 
-func toushaizi() (playerds int, monsterds int) {
-	playerds = rand1.Intn(100)
-	monsterds = rand1.Intn(100)
-
-	return playerds, monsterds
-}
-
 func fight1() {
 	var monsterhp = houshou[0].monsterhp
 	var playerhp = xianshou[0].playerhp
+	var playergjbw string
+	var monstergjbw string
+	var sh int
+	var mi int
+	var pi int
 	fmt.Println("fight1")
 	for {
-		fmt.Println(xianshou[0].playername)
-		fmt.Println(xianshou[0].playername + "瞄准" + gjbw[rand1.Intn(14)] + "准备击发")
-		fmt.Println(houshou[0].monstername + "瞄准" + gjbw[rand1.Intn(14)] + "准备击发")
-		sh := rand1.Intn(10)
+		playergjbw = gjbw[rand1.Intn(len(gjbw))]
+		monstergjbw = gjbw[rand1.Intn(len(gjbw))]
+		fmt.Println(xianshou[0].playername + "瞄准" + playergjbw + "准备击发")
+		fmt.Println(houshou[0].monstername + "瞄准" + monstergjbw + "准备击发")
+		if isYh(playergjbw) {
+			sh = rand1.Intn(10) * 2
+		} else {
+			sh = rand1.Intn(10)
+		}
+
 		fmt.Println(xianshou[0].playername+"击出 , 造成", sh, "点伤害")
-		mi := monsterhp - sh
+		mi = monsterhp - sh
 		monsterhp = mi
 		fmt.Println(houshou[0].monstername, "剩余", monsterhp)
-
-		sh = rand1.Intn(10)
+		if isYh(playergjbw) {
+			sh = rand1.Intn(10) * 2
+		} else {
+			sh = rand1.Intn(10)
+		}
+		if isDead(mi) {
+			fmt.Println(xianshou[0].playername, "死亡 ", houshou[0].monstername, "胜")
+			break
+		}
 		fmt.Println(houshou[0].monstername+"击出 , 造成", sh, "点伤害")
-		pi := playerhp - sh
+		pi = playerhp - sh
 		playerhp = pi
 		fmt.Println(xianshou[0].playername, "剩余", playerhp)
 
-		if monsterhp <= 0 || playerhp <= 0 {
-			if monsterhp <= 0 {
-				fmt.Println("战斗结束", "胜者 :", xianshou[0].playername, "   剩余HP :", playerhp)
-			} else {
-				fmt.Println("战斗结束", "胜者 :", houshou[0].monstername, "   剩余HP :", monsterhp)
-			}
+		if isDead(pi) {
+			fmt.Println(houshou[0].monstername, "死亡 ", xianshou[0].playername, "胜")
 			break
 		}
-
 	}
-
 }
 
 func fight2() {
 	var monsterhp = houshou1[0].playerhp
 	var playerhp = xianshou1[0].monsterhp
+	var playergjbw string
+	var monstergjbw string
+	var sh int
+	var mi int
+	var pi int
 	fmt.Println("fight2")
 	for {
-
-		fmt.Println(xianshou1[0].monstername + "瞄准" + gjbw[rand1.Intn(14)] + "准备击发")
-		fmt.Println(houshou1[0].playername + "瞄准" + gjbw[rand1.Intn(14)] + "准备击发")
-
-		sh := rand1.Intn(10)
+		playergjbw = gjbw[rand1.Intn(len(gjbw))]
+		monstergjbw = gjbw[rand1.Intn(len(gjbw))]
+		fmt.Println(xianshou1[0].monstername + "瞄准" + playergjbw + "准备击发")
+		fmt.Println(houshou1[0].playername + "瞄准" + monstergjbw + "准备击发")
+		if isYh(playergjbw) {
+			sh = rand1.Intn(10) * 2
+		} else {
+			sh = rand1.Intn(10)
+		}
 		fmt.Println(xianshou1[0].monstername+"击出 , 造成", sh, "点伤害")
-		pi := playerhp - sh
+		pi = playerhp - sh
 		playerhp = pi
 		fmt.Println(houshou1[0].playername, "剩余", playerhp)
 
-		sh = rand1.Intn(10)
-		fmt.Println(houshou1[0].playername+"击出 , 造成", sh, "点伤害")
-		mi := monsterhp - sh
-		monsterhp = mi
-		fmt.Println(xianshou1[0].monstername, "剩余", mi)
-
-		if monsterhp <= 0 || playerhp <= 0 {
-			if monsterhp <= 0 {
-				fmt.Println("战斗结束", "胜者 :", houshou1[0].playername, "   剩余HP :", playerhp)
-			} else {
-				fmt.Println("战斗结束", "胜者 :", xianshou1[0].monstername, "   剩余HP :", monsterhp)
-			}
+		if isYh(monstergjbw) {
+			sh = rand1.Intn(10) * 2
+		} else {
+			sh = rand1.Intn(10)
+		}
+		if isDead(pi) {
+			fmt.Println(houshou1[0].playername, "死亡 ", xianshou1[0].monstername, "胜")
 			break
 		}
-
+		fmt.Println(houshou1[0].playername+"击出 , 造成", sh, "点伤害")
+		mi = monsterhp - sh
+		if isYh(playergjbw) {
+			sh = rand1.Intn(10) * 2
+		} else {
+			sh = rand1.Intn(10)
+		}
+		if isDead(mi) {
+			fmt.Println(xianshou1[0].monstername, "死亡 ", houshou1[0].playername, "胜")
+			break
+		}
+		monsterhp = mi
+		fmt.Println(xianshou1[0].monstername, "剩余", mi)
 	}
+}
+
+func isDead(i int) (n bool) {
+	if i <= 0 {
+		return true
+	}
+	return false
+}
+
+func isYh(s string) (n bool) {
+	for i := 0; i < len(yhbw); i++ {
+		if s == gjbw[i] {
+			return true
+		}
+		return false
+	}
+	return true
 }
